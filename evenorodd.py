572545581d1e4affbcaaee42ec5d97a9709aff25
TharuntejaNakkala <tharuntejayadav@gmail.com>
2025-04-03 22:49:53 +0530

    code

--- evenorodd.py
+++ evenorodd.py
@@ -3,3 +3,10 @@ if n % 2 == 0:
     print("even")
 else:
     print("odd")
+
+#REPLACE EVEN NUMBER TO #
+for num in range(1, 101): 
+    if num % 2 == 0:  
+        print("#", end=" ")  
+    else:
+        print(num, end=" ") 
---
+++ squarenums.py
@@ -0,0 +1,5 @@
+
+nums = [1, 2, 3, 4, 5]
+squared_nums = [num ** 2 for num in nums]
+print("Original list:", nums)
+print("Squared list:", squared_nums)
